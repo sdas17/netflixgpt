93546ee06009ebf8badc11244c925685d5fcefb8
sdas171 <sdas1@miraclesoft.com>
2024-09-18 13:25:25 +0530

    add -dumy

--- src/Component/Broweser.js
+++ src/Component/Broweser.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { createContext, useEffect } from 'react'
 import Header from './Header'
+import {options} from '../utiles/userconstant';
+
+// Custom hook for easier context usage
 
 const Broweser = () => {
+
+
+  useEffect(()=>{
+    fetchApiCalling();
+  },[])
+  const url = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+
+  const fetchApiCalling = async () => {
+    try {
+      const response = await fetch(url, options); // Replace "url" with the actual URL and "options" with your fetch options
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json(); // Assuming the response is in JSON format
+      console.log(data); // Handle the data
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  
+  
+
   return (
     <div className="absolute w-screen  px-8  py-7 bg-gradient-to-b from -black z-30">
       <Header/>
--- src/utiles/userconstant.js
+++ src/utiles/userconstant.js
@@ -1,2 +1,9 @@
 export const logo ='https://assets.nflxext.com/ffe/siteui/vlv3/85ff76db-39e5-423a-afbc-97d3e74db71b/null/IN-en-20240909-TRIFECTA-perspective_b22117e0-4610-4d57-a695-20f77d241a4a_large.jpg'
 
+export const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzEyNjllZTI5ZTQxZWRlOTg4YTYyZTUzNjU5OWM4ZiIsIm5iZiI6MTcyNjY0MDM2MS4wOTY2MTgsInN1YiI6IjY2ZTk1MTQyMWJlY2E4Y2UwN2QyYzU0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CGyovXviouoXAuWfBLrQZKykcYNEgkq52spIQo1aEVs'
+    }
+  };
